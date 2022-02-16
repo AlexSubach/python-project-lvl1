c90428c9e3c4e84a66df570503c0dd6fb56511c2
Alex Subach <alex_28.02@mail.ru>
2022-02-16 10:59:00 +0300

    add brain-even and big fix

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 brain-games:
 	poetry run brain-games
+brain-even:
+	poetry run brain-even
 lint:
 	poetry run flake8 brain_games
 restart: install build publish package-reinstall 
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from random import randint
+import prompt
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+
+    def is_even():
+        print('Answer "yes" if the number is even, otherwise answer "no".')
+        count = 0
+        while count < 3:
+            random_numb = randint(1, 1000)
+            print(f'Question: {random_numb}')
+            answer = input('Your answer: ')
+            if random_numb % 2 == 0 and answer == 'yes':
+                print('Correct!')
+                count += 1
+            elif random_numb % 2 != 0 and answer == 'no':
+                print('Correct!')
+                count += 1
+            elif random_numb % 2 == 0 and answer == 'no':
+                print(f"'no' is wrong answer ;(. Correct answer was 'yes'. Let's try again, {name}!")
+                break
+            elif random_numb % 2 != 0 and answer == 'yes':
+                print(f"'yes' is wrong answer ;(. Correct answer was 'no'. Let's try again, {name}!")
+                break
+            if count == 3:
+                print(f'Congratulations, {name}!')
+                break
+            if answer != 'yes' and answer != 'no':
+                print(f'Sorry,"{answer}" this is invalid input!')
+                break
+    is_even()
+
+
+if __name__ == '__main__':
+    main()
